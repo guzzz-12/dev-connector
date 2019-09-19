54366b2cea158e73f032cce17a375d7d35ec0abb
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-19 15:42:06 -0400

    Tomar la data del perfil del usuario actual

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-router.get("/", (req, res) => {
-  res.send("Ruta profiles")
+//Leer perfil del usuario actual
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({user: req.user.id}).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({
+        message: "Theres no profile for that user"
+      })
+    }
+
+    res.json(profile);
+
+  } catch(error) {
+    console.error(error.message);
+    res.status(500).send("Server error")
+  }
 });
 
 module.exports = router;
