933ea563629c2f7a187573fc63efe18987aa4acc
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-19 08:45:32 -0400

    Definidas rutas, instalado dotenv

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
   res.send("API en ejecución!")
 });
 
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 app.listen(port, () => {
   console.log(`Server inicializado en el puerto ${port}`)
 });
