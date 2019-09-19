4034449d5c03c4f5eb822d0a613bef46c2fecb16
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-19 08:01:21 -0400

    Agregada conexión con MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+connectDB();
+
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
