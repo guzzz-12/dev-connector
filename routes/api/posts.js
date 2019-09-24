1ab110ab1e40bda367e4dd8d53244ae65a7d6389
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-24 08:23:48 -0400

    Router para creación de posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const {check, validationResult} = require("express-validator");
 
-router.get("/", (req, res) => {
-  res.send("Ruta posts")
-});
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+const Post = require("../../models/Post");
+
+//Crear posts
+router.post("/",
+  [
+    auth,
+    check("text", "Text is required").not().isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      })
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = {
+        text: req.body.text,
+        name: req.body.name || user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      }
+
+      const post = await Post.create(newPost);
+      res.json(post);
+      
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Server error")
+    }
+  }
+);
 
 module.exports = router;
