bb5de4c60a5aade2832a97dcbfa451c16184c9ec
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-25 08:08:45 -0400

    Rutas a los componentes de login y registro de usuarios

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import "./App.css";
 
 const App = () => {
   return (
-    <React.Fragment>
-      <Navbar />
-      <Landing />
-    </React.Fragment>
+    <BrowserRouter>
+      <React.Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
