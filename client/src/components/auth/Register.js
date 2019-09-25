85c0788f73bea327d0fdce52f2a6db5d7dc8fba7
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-25 08:28:21 -0400

    Estructura y funcionalidad básica de formulario de registro

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,86 @@
-import React from 'react';
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: ""
+  });
+
+  const {name, email, password, passwordConfirm} = formData;
+
+  const onChangeHandler = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if(password !== passwordConfirm) {
+      console.log("Password don't match");
+    } else {
+      console.log(formData);
+    }
+  }
+
   return (
-    <div>
-      Register
-    </div>
+    <React.Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit={submitHandler}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={e => onChangeHandler(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChangeHandler(e)}
+          />
+          <small className="form-text"
+            >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={e => onChangeHandler(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="passwordConfirm"
+            minLength="6"
+            value={passwordConfirm}
+            onChange={e => onChangeHandler(e)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </React.Fragment>
   );
 }
 
