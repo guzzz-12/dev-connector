2398e6d31acadf47d1426c04d9fc8edacc1181c8
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-25 07:49:22 -0400

    Frontend: Commit inicial del frontend

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import "./App.css";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Landing />
+    </React.Fragment>
+  );
+}
+
+export default App;
