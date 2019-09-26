ab428946a98fbcd52fe27633a2aea63911ee9083
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-26 10:24:22 -0400

    Action y reducer para cerrar sesión (Logout)

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL} from "./types";
+import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT} from "./types";
 import {setAlert} from "./alert";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -99,5 +99,12 @@ export const login = (data) => {
         type: LOGIN_FAIL
       })
     }
+  }
+}
+
+//Cerrar sesión (Logout)
+export const logout = () => {
+  return (dispatch) => {
+    dispatch({type: LOGOUT})
   }
 }
