e9f9eaf6bdbd06d376cb8f82393ade63f56c6eca
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-26 10:24:35 -0400

    Funcionalidad para cerrar sesión de usuario
    Se completó la funcionalidad para cerrar sesión de usuario y se condicionó el contenido del menú de navegación del header para mostrar diferentes botones en función de si hay o no usuario logueado

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {connect} from "react-redux";
+import PropTypes from "prop-types";
+import {logout} from "../../actions/auth";
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <a onClick={props.logout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>{" "}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+      <li><Link to="#!">Developers</Link></li>
+      <li><Link to="/register">Register</Link></li>
+      <li><Link to="/login">Login</Link></li>
+    </ul>
+  );
+  
   return (
     <nav className="navbar bg-dark">
       <h1>
@@ -9,13 +31,35 @@ const Navbar = () => {
           <i className="fas fa-code"></i> DevConnector
         </Link>
       </h1>
-      <ul>
-        {/* <li><Link to="/profiles">Developers</Link></li> */}
-        <li><Link to="/register">Register</Link></li>
-        <li><Link to="/login">Login</Link></li>
-      </ul>
+      {!props.auth.loading && (
+        <React.Fragment>
+          {props.auth.isAuthenticated ?
+            authLinks :
+            guestLinks
+          }
+        </React.Fragment>
+      )}     
     </nav>
   );
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.authReducer
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: () => {
+      dispatch(logout())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
