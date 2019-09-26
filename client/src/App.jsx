843def44dffe925d483daa381edc2284900b69d0
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-26 07:57:32 -0400

    Guardar la data del usuario logueado en el store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import {loadUser} from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 import "./App.css";
 
 //Configuración de Redux
 import {Provider} from "react-redux";
 import store from "./store"; 
 
+//Verificar si existe token de usuario en el storage
+//Si existe token, enviarlo al backend para tomar la data del usuario
+if(localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
 const App = () => {
+  //Cargar el usuario cuando el componente se haya creado
+  //Versión de componentDidMount() para componentes funcionales
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, []);
+
   return (
     <Provider store={store}>
       <BrowserRouter>
