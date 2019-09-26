dae04c291472a794f818d4858daccf9e1eeb61a8
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-26 07:15:39 -0400

    Refactorizada funcionalidad (frontend) para registro de usuarios

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,20 +5,27 @@ import {setAlert} from "./alert";
 //Registrar usuario
 export const register = (data) => {
   return async (dispatch) => {
-    const config = {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }
-
-    const body = JSON.stringify(data);
-
     try {
-      const res = await axios.post("/api/users", body, config);
+      const {name, email, password, passwordConfirm} = data;
+      const res = await axios({
+        method: "POST",
+        url: "/api/users",
+        data: {
+          name,
+          email,
+          password,
+          passwordConfirm
+        },
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
       dispatch({
         type: REGISTER_SUCCESS,
         payload: res.data
       });
+
       dispatch(setAlert("Successfully registered", "success"))
 
     } catch (error) {
