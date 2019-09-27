61fc7bc0339c1303daa170197c2fa29a5fe7e686
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-27 07:58:42 -0400

    Estructura básica del Dashboard

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React, {useEffect} from "react";
+import {Link} from "react-router-dom";
 import {connect} from "react-redux";
 import {getCurrentProfile} from "../../actions/profile";
+import Spinner from "../layout/Spinner";
 
 const Dashboard = (props) => {
   useEffect(() => {
@@ -8,16 +10,35 @@ const Dashboard = (props) => {
   }, [])
 
   return (
-    <div>
-      Dashboard
-    </div>
+    props.isLoading && props.profile === null ?
+      <Spinner /> :
+      <React.Fragment>
+        <h1 className="large text-primary">Dashboard</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i>
+          Welcome {props.user && props.user.name}
+        </p>
+        {props.profile !== null ?
+          <React.Fragment>
+            Has profile
+          </React.Fragment> :
+          <React.Fragment>
+            <p>You have not created a profile yet</p>
+            <Link to="/create-profile" className="btn btn-primary my-1">
+              Create Profile
+            </Link>
+          </React.Fragment>
+        }
+      </React.Fragment>
   );
 }
 
 const mapStateToProps = (state) => {
   return {
+    user: state.authReducer.user,
     profile: state.profileReducer.profile,
-    isAuth: state.authReducer.isAuthenticated
+    isAuth: state.authReducer.isAuthenticated,
+    isLoading: state.authReducer.loading
   }
 }
 
