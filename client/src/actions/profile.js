c46b658227bb1058d5010eed934210d191ae006e
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-27 10:47:43 -0400

    Action para crear/editar perfil de usuario

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -28,3 +28,47 @@ export const getCurrentProfile = () => {
     }
   }
 }
+
+//Crear/actualizar el perfil del usuario
+export const createProfile = (formData, history, edit = false) => {
+  return async (dispatch) => {
+    try {
+      const res = await axios({
+        method: "POST",
+        url: "/api/profile",
+        data: {
+          ...formData
+        },
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      });
+      dispatch(setAlert(edit ? "Profile updated!" : "Profile created!", "success"));
+
+      if(!edit) {
+        history.push("/dashboard");
+      }
+
+    } catch (error) {
+      console.log(error.response)
+      const errors = error.response.data.errors;
+      if(errors) {
+        errors.forEach(err => {
+          dispatch(setAlert(err.msg, "danger"))
+        })
+      }
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: error.response.statusText,
+          status: error.response.status
+        }
+      })
+    }
+  }
+}
