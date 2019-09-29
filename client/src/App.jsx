4dca3ea994e8538f2ac74a57a9b7637e7916b855
J3duardo <jesus_69eduardo@hotmail.com>
2019-09-29 07:47:41 -0400

    Ruta y link a la página de los perfiles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import CreateProfile from "./components/profile-form/CreateProfile";
 import EditProfile from "./components/profile-form/EditProfile";
 import AddExperience from "./components/profile-form/AddExperience";
 import AddEducation from "./components/profile-form/AddEducation";
+import Profiles from "./components/profiles/Profiles";
 
 //Configuración de Redux
 import {Provider} from "react-redux";
@@ -48,6 +49,7 @@ const App = () => {
               <PrivateRoute exact path="/edit-profile" component={EditProfile} />
               <PrivateRoute exact path="/add-experience" component={AddExperience} />
               <PrivateRoute exact path="/add-education" component={AddEducation} />
+              <PrivateRoute exact path="/profiles" component={Profiles} />
             </Switch>
           </section>
         </React.Fragment>
