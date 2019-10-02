e225fe0f596d5a7b857758f6d83c5de4a817d2fb
J3duardo <jesus_69eduardo@hotmail.com>
2019-10-02 10:44:49 -0400

    Action y reducer para crear posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {setAlert} from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from "./types";
 
 //Tomar los posts
 export const getPosts = () => {
@@ -109,6 +109,43 @@ export const deletePost = (postId) => {
             status: error.response.status
           }
         })
+      }
+    }
+  }
+}
+
+//Crear post
+export const createPosts = (postContent) => {
+  return async (dispatch) => {
+    try {
+      const res = await axios({
+        method: "POST",
+        url: `/api/posts`,
+        data:{
+          text: postContent
+        },
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      dispatch({
+        type: ADD_POST,
+        payload: res.data
+      });
+
+      dispatch(setAlert("Post created successfully", "success"))
+
+    } catch (error) {
+      if(error.response) {
+        dispatch({
+          type: POST_ERROR,
+          payload: {
+            msg: error.response.statusText,
+            status: error.response.status
+          }
+        })
+        dispatch(setAlert(error.response.data.errors[0].msg, "danger"))
       }
     }
   }
