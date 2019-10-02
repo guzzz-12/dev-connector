45ace5a64ec2bdf9bb88f806b89268e4290b400b
J3duardo <jesus_69eduardo@hotmail.com>
2019-10-02 09:02:09 -0400

    Funcionalidad para borrar posts

--- client/src/components/posts/PostItem.jsx
+++ client/src/components/posts/PostItem.jsx
@@ -2,10 +2,21 @@ import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 import {format} from "date-fns";
 import {connect} from "react-redux";
-import {addLikes, removeLikes} from "../../actions/post";
+import {addLikes, removeLikes, deletePost} from "../../actions/post";
+import Modal from "../confirm-modal/Modal";
 
 const PostItem = (props) => {
   const {_id, text, name, avatar, user, likes, comments, date} = props.post;
+
+  const [toggleModal, setToggleModal] = useState(false);
+
+  const toggleModalHandler = (val) => {
+    setToggleModal(val)
+  }
+
+  const deletePostHandler = (id) => {
+    props.deletePost(id);
+  }
 
   const [isLiked, setIsLiked] = useState(false);
 
@@ -26,56 +37,66 @@ const PostItem = (props) => {
   }, [props.auth.user, likes]);
 
   return (
-    <div className="post bg-white p-1 my-1">
-      <div>
-        <Link to={`/profiles/user/${user}`}>
-          <img
-            className="round-img"
-            src={avatar}
-            alt={name}
-          />
-          <h4>{name}</h4>
-        </Link>
-      </div>
-      <div>
-        <p className="my-1">{text}</p>
-        {date && 
-          <p className="post-date">
-            Posted on {format(new Date(date), "yyyy-MM-dd")}
-          </p>        
-        }
-        <button
-          type="button"
-          id="addLikeBtn"
-          className="btn btn-light"
-          onClick={() => props.addLike(_id)}
-          disabled={isLiked}
-        >
-          <i className="fas fa-thumbs-up"></i>
-          {" "}<span>{likes.length}</span>
-        </button>
-        <button 
-          type="button"
-          id="addLikeBtn"
-          className="btn btn-light"
-          onClick={() => props.removeLike(_id)}
-          disabled={!isLiked}
-        >
-          <i className="fas fa-thumbs-down"></i>
-        </button>
-        <Link to={`/posts/${_id}`} className="btn btn-primary">
-          Discussion{" "}<span className='comment-count'>{comments.length}</span>
-        </Link>
-        {props.auth.isAuthenticated && props.auth.user._id === user && 
-          <button      
+    <React.Fragment>
+      <Modal
+        show={toggleModal}
+        toggleModal={toggleModalHandler}
+        action={deletePostHandler}
+        id={_id}
+      />
+      <div className="post bg-white p-1 my-1">
+        <div>
+          <Link to={`/profiles/user/${user}`}>
+            <img
+              className="round-img"
+              src={avatar}
+              alt={name}
+            />
+            <h4>{name}</h4>
+          </Link>
+        </div>
+        <div>
+          <p className="my-1">{text}</p>
+          {date && 
+            <p className="post-date">
+              Posted on {format(new Date(date), "yyyy-MM-dd")}
+            </p>        
+          }
+          <button
             type="button"
-            className="btn btn-danger"
+            id="addLikeBtn"
+            className="btn btn-light"
+            onClick={() => props.addLike(_id)}
+            disabled={isLiked}
           >
-            <i className="fas fa-times"></i>
-          </button>        
-        }
+            <i className="fas fa-thumbs-up"></i>
+            {" "}<span>{likes.length}</span>
+          </button>
+          <button 
+            type="button"
+            id="addLikeBtn"
+            className="btn btn-light"
+            onClick={() => props.removeLike(_id)}
+            disabled={!isLiked}
+          >
+            <i className="fas fa-thumbs-down"></i>
+          </button>
+          <Link to={`/posts/${_id}`} className="btn btn-primary">
+            Discussion{" "}<span className='comment-count'>{comments.length}</span>
+          </Link>
+          {props.auth.isAuthenticated && props.auth.user._id === user && 
+            <button      
+              type="button"
+              className="btn btn-danger"
+              title="Delete post"
+              onClick={() => toggleModalHandler(true)}
+            >
+              <i className="fas fa-times"></i>
+            </button>        
+          }
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 }
 
@@ -92,6 +113,9 @@ const mapDispatchToProps = (dispatch) => {
     },
     removeLike: (postId) => {
       dispatch(removeLikes(postId))
+    },
+    deletePost: (postId) => {
+      dispatch(deletePost(postId))
     }
   }
 }
