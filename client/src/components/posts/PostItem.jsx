fb9fc2c7c5949abc14aa6019bb4a37769a5da9d4
J3duardo <jesus_69eduardo@hotmail.com>
2019-10-02 08:10:01 -0400

    Funcionalidad para agregar y remover likes en los posts

--- client/src/components/posts/PostItem.jsx
+++ client/src/components/posts/PostItem.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 import {format} from "date-fns";
 import {connect} from "react-redux";
+import {addLikes, removeLikes} from "../../actions/post";
 
 const PostItem = (props) => {
   const {_id, text, name, avatar, user, likes, comments, date} = props.post;
+
+  const [isLiked, setIsLiked] = useState(false);
+
+  //Actualizar el state al montar el componente y al cambiar el número de likes
+  useEffect(() => {
+    //Tomar las id's de los usuarios que dieron like
+    const postLikes = props.post && props.post.likes.map(like => like.user);  
+
+    //Chequear si el usuario dio like al post 
+    const checkIfLiked = props.auth.user ? postLikes.includes(props.auth.user._id) : false;
+
+    //Actualizar el state
+    if (checkIfLiked) {
+      setIsLiked(true)
+    } else {
+      setIsLiked(false)
+    }
+  }, [props.auth.user, likes]);
 
   return (
     <div className="post bg-white p-1 my-1">
@@ -25,11 +44,23 @@ const PostItem = (props) => {
             Posted on {format(new Date(date), "yyyy-MM-dd")}
           </p>        
         }
-        <button type="button" className="btn btn-light">
+        <button
+          type="button"
+          id="addLikeBtn"
+          className="btn btn-light"
+          onClick={() => props.addLike(_id)}
+          disabled={isLiked}
+        >
           <i className="fas fa-thumbs-up"></i>
           {" "}<span>{likes.length}</span>
         </button>
-        <button type="button" className="btn btn-light">
+        <button 
+          type="button"
+          id="addLikeBtn"
+          className="btn btn-light"
+          onClick={() => props.removeLike(_id)}
+          disabled={!isLiked}
+        >
           <i className="fas fa-thumbs-down"></i>
         </button>
         <Link to={`/posts/${_id}`} className="btn btn-primary">
@@ -54,4 +85,15 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(PostItem);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addLike: (postId) => {
+      dispatch(addLikes(postId))
+    },
+    removeLike: (postId) => {
+      dispatch(removeLikes(postId))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostItem);
