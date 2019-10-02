ca429233f122098c31fe67ba032b67694dd96c1e
J3duardo <jesus_69eduardo@hotmail.com>
2019-10-02 09:00:21 -0400

    Modal para confirmar acciones

---
+++ client/src/components/confirm-modal/Modal.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./modal.css";
+
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {props.show &&
+        <div className="modal-container">
+          <div className="modal-content">
+            <h2 className="modal-title text-primary">Are you sure?</h2>
+            <div className="modal-buttons">
+              <button
+                onClick={() => {
+                    props.action(props.id);
+                    props.toggleModal(false)
+                  }
+                }
+                className="btn btn-danger"
+              >
+                OK
+              </button>
+              <button onClick={() => props.toggleModal(false)} className="btn btn-primary">Cancel</button>
+            </div>
+          </div>
+        </div>      
+      }
+    </React.Fragment>
+  );
+}
+
+export default Modal;
