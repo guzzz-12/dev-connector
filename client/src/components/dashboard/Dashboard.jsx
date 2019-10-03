702b1c4df1c7be9b5cdc6082cd0bedf975649bf8
J3duardo <jesus_69eduardo@hotmail.com>
2019-10-03 08:12:21 -0400

    Mostrar modal de confirmación al eliminar la cuenta

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import {connect} from "react-redux";
 import {getCurrentProfile, deleteUserAccount} from "../../actions/profile";
@@ -6,45 +6,67 @@ import Spinner from "../layout/Spinner";
 import DashboardActions from "./DashboardActions";
 import Experience from "./Experience";
 import Education from "./Education";
+import Modal from "../confirm-modal/Modal";
 
 const Dashboard = (props) => {
   useEffect(() => {
     props.getCurrentProfile()
-  }, [])
+  }, []);
+
+  const [toggleModal, setToggleModal] = useState(false);
+
+  const toggleModalHandler = (val) => {
+    setToggleModal(val)
+  }
+
+  const deleteAccountHandler = () => {
+    props.deleteUserAccount();
+  }
 
   return (
-    props.isLoading && props.profile === null ?
-      <Spinner /> :
-      <React.Fragment>
-        <h1 className="large text-primary">Dashboard</h1>
-        <p className="lead">
-          <i className="fas fa-user"></i>
-          Welcome {props.user && props.user.name}
-        </p>
-        {props.profile !== null ?
-          <React.Fragment>
-            <DashboardActions />
-            {props.profile.experience.length > 0 && (
-              <Experience experience={props.profile.experience} />
-            )}
-            {props.profile.education.length > 0 && (
-              <Education education={props.profile.education} />
-            )}
-            <div className="my-2">
-              <button onClick={() => props.deleteUserAccount()} className="btn btn-danger">
+    <React.Fragment>
+      <Modal
+        show={toggleModal}
+        toggleModal={toggleModalHandler}
+        action={deleteAccountHandler}
+        id={null}
+      />
+      {props.isLoading && props.profile === null ?
+        <Spinner /> :
+        <React.Fragment>
+          <h1 className="large text-primary">Dashboard</h1>
+          <p className="lead">
+            <i className="fas fa-user"></i>
+            Welcome {props.user && props.user.name}
+          </p>
+          {props.profile !== null ?
+            <React.Fragment>
+              <DashboardActions />
+              {props.profile.experience.length > 0 && (
+                <Experience experience={props.profile.experience} />
+              )}
+              {props.profile.education.length > 0 && (
+                <Education education={props.profile.education} />
+              )}
+              <button onClick={() => toggleModalHandler(true)} className="btn btn-danger">
                 <i className="fas fa-user-minus"></i>
                 {" "}Delete My Account
               </button>
-            </div>
-          </React.Fragment> :
-          <React.Fragment>
-            <p>You have not created a profile yet</p>
-            <Link to="/create-profile" className="btn btn-primary my-1">
-              Create Profile
-            </Link>
-          </React.Fragment>
-        }
-      </React.Fragment>
+            </React.Fragment> :
+            <React.Fragment>
+              <p>You have not created a profile yet</p>
+              <Link to="/create-profile" className="btn btn-primary my-1">
+                Create Profile
+              </Link>
+              <button onClick={() => toggleModalHandler(true)} className="btn btn-danger">
+                <i className="fas fa-user-minus"></i>
+                {" "}Delete My Account
+              </button>
+            </React.Fragment>
+          }
+        </React.Fragment>
+      }
+    </React.Fragment>
   );
 }
 
