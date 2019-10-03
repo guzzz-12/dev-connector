9c988f88886932b4ced6b534d76b75ecd77705d1
J3duardo <jesus_69eduardo@hotmail.com>
2019-10-03 09:59:19 -0400

    Modal refactorizado
    Se refactorizó el modal para renderizar su contenido según el tipo de action a ejecutar.

--- client/src/components/confirm-modal/Modal.jsx
+++ client/src/components/confirm-modal/Modal.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import "./modal.css";
 
 const Modal = (props) => {
+  const [password, setPassword] = useState("");
+
   return (
     <React.Fragment>
       {props.show &&
         <div className="modal-container">
           <div className="modal-content">
             <h2 className="modal-title text-primary">Are you sure?</h2>
+            
+            {props.actionName === "deleteAccount" && 
+              <form className="form" onSubmit={(e) => e.preventDefault()}>
+                <div className="form-group" style={{marginTop: "0"}}>
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    minLength="6"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+              </form>          
+            }
+
             <div className="modal-buttons">
               <button
                 onClick={() => {
-                    props.action(props.id ? props.id : null);
-                    props.toggleModal(false)
+                    if(props.actionName === "deleteAccount") {
+                      props.action(password, props.userPassword);
+                      props.toggleModal(false);
+                      return;
+                    } else {
+                      props.action(props.id && props.id);
+                      props.toggleModal(false)
+                    }
                   }
                 }
                 className="btn btn-danger"
